d7cce02f489502a200af6493cf615e42d9893c78
CR-Martin <martinconcettilol@gmail.com>
2026-04-26 20:39:43 -0300

    Player rotation

--- Captain Robot/Assets/Scrips/Player/PlayerMovement.cs
+++ Captain Robot/Assets/Scrips/Player/PlayerMovement.cs
@@ -29,12 +29,13 @@ public class PlayerMovement : MonoBehaviour
         if (_currentMovement.magnitude >= 1f)
         {
             rigidBody.MovePosition((Vector3)transform.position + _currentMovement * 10 * Time.deltaTime);
+            float Angle = Mathf.Atan2(_currentMovement.x, _currentMovement.z) * Mathf.Rad2Deg;
+            float Smooth = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref Myfloat, 0.1f);
+            transform.rotation = Quaternion.Euler(0, Smooth, 0);
+
 
         }
 
-        float Angle = Mathf.Atan2(_currentMovement.x, _currentMovement.z) * Mathf.Rad2Deg;
-        float Smooth = Mathf.SmoothDampAngle(transform.eulerAngles.y, Angle, ref Myfloat, 0.1f); 
-        transform.rotation = Quaternion.Euler(0, Smooth, 0); 
     }
 
     public void Movement(Vector2 direction)
