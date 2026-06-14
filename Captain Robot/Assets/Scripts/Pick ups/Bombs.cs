e7cb2200dcbe875dbd47982bb7fc2cfcaebd85ef
CR-Martin <martinconcettilol@gmail.com>
2026-06-14 18:35:31 -0300

    Pause and destructible objects

--- Captain Robot/Assets/Scripts/Pick ups/Bombs.cs
+++ Captain Robot/Assets/Scripts/Pick ups/Bombs.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 public class Bombs : Item
 {
     private float forceThrow = 100;
+    private int maxTimer = 3;
+    private float currentTime = 0;
+
     public override Item DoAction()
     {
         transform.SetParent(null);
@@ -10,6 +14,8 @@ public class Bombs : Item
         rb.useGravity = true;
 
         rb.AddForce(transform.forward * forceThrow);
+        StartCoroutine("Explode");
+
         return this;
     }
 
@@ -22,5 +28,34 @@ public class Bombs : Item
     public override void KinematicState(bool state)
     {
         rb.isKinematic = state;
+    }
+
+    IEnumerator Explode()
+    {
+
+        Debug.Log("Entramos");
+        while (currentTime < maxTimer)
+        {
+            currentTime += Time.deltaTime;
+
+        }
+        Debug.Log("Seguimos");
+
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 2f, -transform.up, 5f);
+        IDamageable isHit;
+
+        foreach (RaycastHit colliders in hits)
+        {
+            Debug.Log(colliders.transform.gameObject);
+
+            if (colliders.transform.GetComponent<IDamageable>() != null)
+            {
+                Debug.Log("Dañlo");
+                isHit = colliders.transform.GetComponent<IDamageable>();
+                isHit.TakeDamage();
+            }
+        }
+
+        yield return null;
     }
 }
